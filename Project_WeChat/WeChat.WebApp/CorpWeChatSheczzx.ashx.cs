fa6e4cd5e6ae1c22ee2af1bd118ad6cc584016b1
Len <liuzhen0927@gmail.com>
2017-05-22 10:42:27 +0800

    modified by Len 2017/05/22

--- Project_WeChat/WeChat.WebApp/CorpWeChatSheczzx.ashx.cs
+++ Project_WeChat/WeChat.WebApp/CorpWeChatSheczzx.ashx.cs
@@ -93,23 +93,23 @@ namespace WeChat.WebApp
             {
 
 
-                CorpSendMsgText msg = new CorpSendMsgText("教委办公室：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=委办  \n发展规划处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=规划 \n人事处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=人事 \n财务处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=财务 \n基教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=基教 \n职教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=职教 \n高教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=高教 \n学生处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=学生 \n科技处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=科技 \n体卫艺科处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=体卫艺科 \n国际交流处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=国交 \n后勤保卫处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=后保 \n政策法规处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=法规 \n语言文字处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=语言文字 \n审计处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=审计 \n德育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=德育 \n督导室：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=督导 \n终身教育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=终教 \n民办教育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=民教 \n青保处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=青保！ \n", instanse.FromUserName, agentid);
+                CorpSendMsgText msg = new CorpSendMsgText("教委办公室：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=委办  \n发展规划处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=规划 \n人事处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=人事 \n财务处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=财务 \n基教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=基教 \n职教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=职教 \n高教处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=高教 \n学生处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=学生 \n科技处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=科技 \n体卫艺科处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=体卫艺科 \n国际交流处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=国交 \n后勤保卫处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=后保 \n政策法规处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=法规 \n语言文字处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=语言文字 \n审计处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=审计 \n德育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=德育 \n督导室：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=督导 \n终身教育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=终教 \n民办教育处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=民教 \n青保处：http://www.shmecsfa.shec.edu.cn/Login/Login?dcode=青保 \n", instanse.FromUserName, agentid);
                 
                 corpCore.SendMsg(msg); 
             }
-            else if ("21".Equals(instanse.EventKey))
-            {
-                NewsMain nm1 = new NewsMain();
-                nm1.title = "申报单位操作说明";
-                //nm1.picurl = "http://wx.seaskysh.com.cn/SeaskyWechatCent/material/封面.png";
-                nm1.picurl = "https://qy.weixin.qq.com/cgi-bin/getmediadata?token=1182936902&lang=zh_CN&type=image&name=%E5%B0%81%E9%9D%A2.png&media_id=2UKhHwnp8zOZgyWtKNf-8uVClA95JZY1UQZ8BvymIj0qzrK3GhdDycqYbThehIJNj";
-                nm1.url = "http://h5.wps.cn/p/226e667e.html";
-                CorpSendMsgNews msg = new CorpSendMsgNews();
-                msg.agentid = agentid;
-                msg.touser = instanse.FromUserName; 
-                msg.news.articles.Add(nm1); 
-                corpCore.SendMsg(msg);
-            }
+            //else if ("21".Equals(instanse.EventKey))
+            //{
+            //    NewsMain nm1 = new NewsMain();
+            //    nm1.title = "申报单位操作说明";
+            //    //nm1.picurl = "http://wx.seaskysh.com.cn/SeaskyWechatCent/material/封面.png";
+            //    nm1.picurl = "https://qy.weixin.qq.com/cgi-bin/getmediadata?token=1182936902&lang=zh_CN&type=image&name=%E5%B0%81%E9%9D%A2.png&media_id=2UKhHwnp8zOZgyWtKNf-8uVClA95JZY1UQZ8BvymIj0qzrK3GhdDycqYbThehIJNj";
+            //    nm1.url = "http://h5.wps.cn/p/226e667e.html";
+            //    CorpSendMsgNews msg = new CorpSendMsgNews();
+            //    msg.agentid = agentid;
+            //    msg.touser = instanse.FromUserName; 
+            //    msg.news.articles.Add(nm1); 
+            //    corpCore.SendMsg(msg);
+            //}
             else if("22".Equals(instanse.EventKey))
             {
                  
