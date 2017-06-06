d29e504cad5ca62b91a241488c0480c3d8bc263f
Len <Pirlol@163.com>
2017-06-06 23:53:07 +0800

    modified by Len 2017/06/06

---
+++ Project_WeChat/WeChat.WebPage/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WeChat.WebPage
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+                        "~/Scripts/jquery-1.*"));
+            bundles.Add(new StyleBundle("~/Css").Include(
+                        "~/Css/weui.css"));
+        }
+    }
+}
