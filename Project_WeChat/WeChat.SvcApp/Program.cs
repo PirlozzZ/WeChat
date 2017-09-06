800c30ad283f425fc21e605337f96e528f00036a
Len <liuzhen0927@gmail.com>
2017-09-06 17:16:48 +0800

    modified by Len

---
+++ Project_WeChat/WeChat.SvcApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WeChat.SvcApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new SvcPub()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Project_WeChat/WeChat.SvcApp/SvcPub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WeChat.SvcApp
+{
+    public partial class SvcPub : ServiceBase
+    {
+        static log4net.ILog log = log4net.LogManager.GetLogger("Log.Logging");//获取一个日志记录器 
+        private System.Timers.Timer _timerOverDayHourFroze;
+        public SvcPub()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timerOverDayHourFroze = new System.Timers.Timer();
+            _timerOverDayHourFroze.Interval = double.Parse(ConfigurationManager.AppSettings["Interval"].ToString());
+            _timerOverDayHourFroze.Enabled = true;
+            _timerOverDayHourFroze.Start();
+            _timerOverDayHourFroze.Elapsed += new System.Timers.ElapsedEventHandler(timerOverDayHourFroze_Elapsed); ;
+            log.Info(string.Format("【服务开始】"));
+        }
+
+        protected override void OnStop()
+        {
+            log.Info(string.Format("【服务结束】"));
+        }
+
+        private void timerOverDayHourFroze_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+        }
+    }
+}
