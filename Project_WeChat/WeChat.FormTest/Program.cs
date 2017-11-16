ff1e78a452c093666f2249d5edb365ae6a055041
Len <liuzhen0927@gmail.com>
2017-11-16 17:37:35 +0800

    modified by len

--- Project_WeChat/WeChat.FormTest/Program.cs
+++ Project_WeChat/WeChat.FormTest/Program.cs
@@ -14,6 +14,7 @@ namespace WeChat.FormTest
         [STAThread]
         static void Main()
         {
+            log4net.Config.XmlConfigurator.Configure();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
